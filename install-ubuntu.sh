5fc70f4f14a585079ad9a4048140df6ce6c1b29a
Swapnil Kulkarni <me@coolsvap.net>
2017-09-15 10:43:44 -0600

    Add initial ubuntu Vagrantfile for ubuntu
    Signed-off-by: Swapnil Kulkarni <me@coolsvap.net>

---
+++ install-ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Source: http://kubernetes.io/docs/getting-started-guides/kubeadm/
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
