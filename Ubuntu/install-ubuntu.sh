d9049027d990715e308ba9a2fbbde7e7d04181fd
Swapnil Kulkarni <me@coolsvap.net>
2017-09-21 10:13:00 +0530

    Updated Ubuntu vagrantfile for customizations
    Change-Id: I56c4639032f9cbb8ea61bf8f273cc27b0f3aef99
    Signed-off-by: Swapnil Kulkarni <me@coolsvap.net>

--- Ubuntu/install-ubuntu.sh
+++ Ubuntu/install-ubuntu.sh
@@ -10,3 +10,13 @@ apt-get update
 apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
 systemctl enable kubelet && systemctl start kubelet
 systemctl enable docker && systemctl start docker
+
+CGROUP_DRIVER=$(sudo docker info | grep "Cgroup Driver" | awk '{print $3}')
+
+sed -i "s|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--cgroup-driver=$CGROUP_DRIVER |g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+sed -i 's/10.96.0.10/10.3.3.10/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+systemctl daemon-reload
+
+systemctl stop kubelet && systemctl start kubelet
