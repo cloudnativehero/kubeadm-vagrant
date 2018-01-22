039897f874fbc0f4c6e6d36c8ad6ccb9bc4216bf
Swapnil Kulkarni <me@coolsvap.net>
2018-01-22 11:23:51 +0530

    Update Vagrantfile and install-centos.sh

--- CentOS/install-centos.sh
+++ CentOS/install-centos.sh
@@ -10,8 +10,13 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
         https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
-setenforce 0
-yum install -y kubelet kubeadm docker
+
+yum install -y net-tools screen tree telnet kubelet kubeadm docker --nogpgcheck
 systemctl enable kubelet && systemctl start kubelet
 systemctl enable docker && systemctl start docker
+
+setenforce 0
+sed -i 's/SELINUX=enforcing/SELINUX=enforcing/g' /etc/selinux/config
+swapoff -a
+sed -i '/swap/s/^/#/g' /etc/fstab
 echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
