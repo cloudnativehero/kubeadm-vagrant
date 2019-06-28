c6bd45bb16ad52dcaa192f9fe203f02a94181dd0
Swapnil Kulkarni <coolsvap@gmail.com>
2019-06-28 10:36:51 +0530

    Update install-ubuntu.sh

--- Ubuntu/install-ubuntu.sh
+++ Ubuntu/install-ubuntu.sh
@@ -2,7 +2,10 @@
 
 # Source: http://kubernetes.io/docs/getting-started-guides/kubeadm/
 
-apt-get erase -y docker.io kubelet kubeadm kubectl kubernetes-cni
+apt-get remove -y docker.io kubelet kubeadm kubectl kubernetes-cni containerd*
+apt-get autoremove -y
+systemctl daemon-reload
+
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
